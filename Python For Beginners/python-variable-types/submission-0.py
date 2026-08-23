4876ca914ee7e62017b9799d9bdb9ee427235ade
nirvana47 <nirvana47@gmail.com>
2026-08-23 04:30:59 -0700

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+integer_type = 10
+float_type = 2.31
+bool_type = True
+string_type = "yo"
+list_type = [2, 3, 6]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
