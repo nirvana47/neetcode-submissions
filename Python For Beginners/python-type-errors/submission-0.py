9f923b2e6ddadf94c2490013a7010b8f3def035b
nirvana47 <nirvana47@gmail.com>
2026-08-24 15:59:06 -0700

    Add: python-type-errors - submission-0

---
+++ Python For Beginners/python-type-errors/submission-0.py
@@ -0,0 +1,11 @@
+variable = "hello"
+
+# Don't modify the code below this line
+variable = "10"
+print(variable)
+print(type(variable))
+
+variable = int(variable)
+
+print(variable)
+print(type(variable))
