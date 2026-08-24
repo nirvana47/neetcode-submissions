cda255d21bbec6826d1aef31ccf0876c8b7390f2
nirvana47 <nirvana47@gmail.com>
2026-08-24 15:56:38 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.1
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "ten"
+print(type(variable))
+variable = ['t', 'e', 'n']
+print(type(variable))
